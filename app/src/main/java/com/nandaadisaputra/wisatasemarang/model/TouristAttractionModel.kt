1238ca54112dd9ba4d05dabff0c22a0535ef23d2
Nanda Adisaputra <nandaadisaputra18@gmail.com>
2025-06-14 09:04:28 +0700

    Initial commit

---
+++ app/src/main/java/com/nandaadisaputra/wisatasemarang/model/TouristAttractionModel.kt
@@ -0,0 +1,7 @@
+package com.nandaadisaputra.wisatasemarang.model
+
+data class TouristAttractionModel(
+    val image: Int, // Menyimpan ID gambar yang akan digunakan untuk menampilkan gambar pariwisata.
+    val title: String, // Menyimpan judul atau nama dari objek pariwisata.
+    val description: String // Menyimpan deskripsi atau informasi tambahan tentang objek pariwisata.
+)
